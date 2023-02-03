16c6daf483bac1a5e1816e20d0b4f4f1558933df
kekevids134 <aydenlim105116@gmail.com>
2023-02-03 15:08:22 +0800

    fixes

---
+++ tokiilo.js
@@ -0,0 +1,18 @@
+function change_eng(){;
+    document.getElementById("txt0").innerHTML = "Hi! I'm jan Kikili, and this is my Github page.";
+    document.getElementById("txt1").innerHTML = "poki soweli, a toki pona omega layers mod";
+    document.getElementById("txt2").innerHTML = "The Baba Tree: a prestige tree mod(currently broken, please don't open)";
+    document.getElementById("txt3").innerHTML = "Ba I Yu: a Baba Is You levelpack";
+    document.getElementById("btn0").style.display = "none";
+    document.getElementById("btn1").style.display = "inline";
+}
+function change_tp(){;
+    document.getElementById("txt0").innerHTML = "toki! mi jan Kikili. ni li lipu Kita mi.";
+    document.getElementById("txt1").innerHTML = "poki soweli: musi nanpa lon toki pona";
+    document.getElementById("txt2").innerHTML = "kasi Papa: musi nanpa(tenpo ni la ni li pakala, o open ala e ni)";
+    document.getElementById("txt3").innerHTML = "musi Pa A Ju: kulupu poki lon musi Papa Li Sina(anu Papa Isu Ju)";
+    document.getElementById("btn0").style.display = "inline";
+    document.getElementById("btn1").style.display = "none";
+}
+
+
