ab2a0c4d2e5931fa7f6e98a1a051cb15b198bd2c
jan Kikili <aydenlim105116@gmail.com>
2024-07-05 21:57:33 +1200

    there i finally updated it

--- toki_ilo.js
+++ toki_ilo.js
@@ -1,24 +1,22 @@
 var showSubmit = false
 function change_eng(){;
     document.getElementById("txt0").innerHTML = "Hi! I'm jan Kikili, and this is my Github page. mu!";
-    document.getElementById("txt1").innerHTML = "poki soweli, a toki pona omega layers mod";
-    document.getElementById("txt2").innerHTML = "The Baba Tree: a prestige tree mod(currently broken, please don't open)";
     document.getElementById("txt3").innerHTML = "Ba I Yu: a Baba Is You levelpack";
     document.getElementById("btn0").style.display = "none";
     document.getElementById("btn1").style.display = "inline";
     document.getElementById("txt4").innerHTML = "RattlesnakeOS(python project)"
     document.getElementById("txt5").innerHTML = "My friend's website!"
     document.getElementById("txt6").innerHTML = "My Youtube channel"
+    document.getElementById("txt7").innerHTML = "Antimatter Dimensions But It Is Very Puzzling - LC8 guide"
 }
 function change_tp(){;
     document.getElementById("txt0").innerHTML = "toki! mi jan Kikili. ni li lipu Kita mi. mu!";
-    document.getElementById("txt1").innerHTML = "poki soweli: musi nanpa lon toki pona";
-    document.getElementById("txt2").innerHTML = "kasi Papa: musi nanpa(tenpo ni la ni li pakala, o open ala e ni)";
     document.getElementById("txt3").innerHTML = "musi Pa A Ju: kulupu poki lon musi Papa Li Sina(anu Papa Isu Ju)";
     document.getElementById("btn0").style.display = "inline";
     document.getElementById("btn1").style.display = "none";
     document.getElementById("txt4").innerHTML = "lipu RattlesnakeOS(toki ilo)"
     document.getElementById("txt5").innerHTML = "lipu Kita pi jan pona mi!"
     document.getElementById("txt6").innerHTML = "lipu Jutu mi"
+    document.getElementById("txt7").innerHTML = "musi Ansimata Simenson Taso Ona Li Musi Pi Sona Wawa - nasin pi pini pona e lili wawa LC8"
 }
 
